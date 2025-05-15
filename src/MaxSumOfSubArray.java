c4c47aac87fef6649544424313925abf5a0e1594
sindushekar <shashank.indushekar@dharbor.com>
2025-05-15 22:18:48 +0530

    Coding Practice of Java

--- src/MaxSumOfSubArray.java
+++ src/MaxSumOfSubArray.java
@@ -1,3 +1,5 @@
+import java.util.List;
+
 public class MaxSumOfSubArray {
     public static void main(String[] args) {
         int [] arr = {2, 1, 5, 1, 3, 2};
@@ -15,7 +17,7 @@ public class MaxSumOfSubArray {
                 Instead of summing the entire subarray each time, you remove the old element (leftmost) and add the new element (rightmost) to the sum.
 
                 Efficiency: This avoids re-summing the entire subarray every time.*/
-        //Now use sliding tatics in this we need to remove the left most value with currentsum and add it with next next right value
+        //Now use sliding statics in this we need to remove the left most value with currentsum and add it with next next right value
         for(int i=subArraySize; i<arr.length;i++){
             currentSum = currentSum - arr[i-subArraySize] + arr[i]; // Here we are removing the left most value and adding the right next value
             maxSum = Math.max(maxSum, currentSum);// update the max sum with max
